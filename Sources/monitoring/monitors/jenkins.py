7522d7b23c9f8573c3631337a68a9325b47f98b5
Ran Tavory <rantav@gmail.com>
2014-06-02 10:07:45 +0300

    Improve jenkins reliability - return Failure in case we can't connect

--- Sources/monitoring/monitors/jenkins.py
+++ Sources/monitoring/monitors/jenkins.py
@@ -1,4 +1,4 @@
-from monitoring import Time, Rule, Success
+from monitoring import Time, Rule, Success, Failure
 
 try:
 	from jenkinsapi.jenkins import Jenkins as JenkinsApi
@@ -8,19 +8,22 @@ except ImportError:
 class Jenkins(Rule):
 
 	def __init__(self, server='localhost:8080', user=None, passw=None,
-	    monitor_queue=None, freq=Time.m(1), fail=(), success=()):
+							 monitor_queue=None, freq=Time.m(1), fail=(), success=()):
 		Rule.__init__(self, freq, fail, success)
 		self.server = server
 		self.action = None
 		if monitor_queue:
 			self.action = 'monitor_queue:%s' % monitor_queue
-		self.jenkins = JenkinsApi(server, user, passw) if JeninsApi or None
+		self.jenkins = JenkinsApi(server, user, passw) if JenkinsApi else None
 
 	def run(self):
 		Rule.run(self)
 		assert self.jenkins, "jenkinsapi module is not  installed"
 		if self.action == 'monitor_queue:global':
-			return Success(len(self.jenkins.get_queue().keys()))
+			try:
+				return Success(len(self.jenkins.get_queue().keys()))
+			except Exception as e:
+				return Failure("Error connecting to jenkins. %s" % e)
 		else:
 			raise Exception("Unknown action {action}".format(action=self.action))
 
