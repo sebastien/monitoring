8ebda2bd1399fdee2f29e4d83257993bbc7b0582
Sebastien Pierre <sebastien.pierre@gmail.com>
2015-04-09 14:41:22 -0400

    Refactored `daemon` module to `service`, add Service base class

--- Sources/monitoring/service.py
+++ Sources/monitoring/service.py
@@ -6,13 +6,92 @@
 # License           :   Revised BSD Licensed
 # -----------------------------------------------------------------------------
 # Creation date     :   29-Dec-2014
-# Last mod.         :   29-Dec-2014
+# Last mod.         :   09-Apr-2015
 # -----------------------------------------------------------------------------
 
-import argparse, time
+import os, time, sys, copy
 from   monitoring import Tmux
 
-class TmuxDaemon:
+# -----------------------------------------------------------------------------
+#
+# SERVICE
+#
+# -----------------------------------------------------------------------------
+
+class Service(object):
+	"""A minimal class to implement services that support start/stop/status
+	directives."""
+
+	Instance      = None
+	CONFIGURATION = {}
+
+	@classmethod
+	def Ensure( cls ):
+		if not cls.Instance: cls.Instance = cls()
+		return cls.Instance
+
+	@classmethod
+	def Run( cls, *args ):
+		if not args or len(args) == 0: args = sys.argv[1:]
+		d         = cls.Ensure()
+		args      = args or ["start"]
+		directive = args[0]
+		args      = args[1:]
+		lines     = d.lines
+		if hasattr(d, directive):
+			result = getattr(d, directive)(*args)
+			# If the directive did not output anything, we print its output
+			if d.lines == lines:
+				d.out("{0}: {1}".format(directive, d.format(result)))
+		else:
+			raise Exception("Directive not found in daemon {1}: {0}".  format(directive, d))
+
+	def __init__( self, config=None ):
+		"""Initializes the service with the given `config`uration, which
+		can be a path (string) or a dictionary of values"""
+		self.lines = 0
+		if not config:
+			config_path = self.__class__.__name__.rsplit(".", 1)[-1].lower() + ".json"
+			if os.path.exists(config_path):
+				with open(config_path) as f:
+					config = json.load(f)
+			else:
+				config = copy.deepcopy(self.CONFIGURATION)
+		elif isinstance(config, str):
+			with open(config_path) as f:
+				config = json.load(f)
+		self.config = config
+
+	def out( self, *args ):
+		for a in args:
+			sys.stdout.write(str(a))
+		sys.stdout.write("\n")
+		self.lines += 1
+		return self
+
+	def format( self, value ):
+		return value
+
+	def start( self ):
+		pass
+
+	def stop( self ):
+		pass
+
+	def status( self ):
+		pass
+
+	def restart( self ):
+		self.stop()
+		self.start()
+
+# -----------------------------------------------------------------------------
+#
+# TMUX SERVICE
+#
+# -----------------------------------------------------------------------------
+
+class TmuxService(Service):
 	"""Creates long-running processed running within a dedicated Tmux
 	session. This allows to interactively query/manipulate
 	processes within a tmux shell."""
@@ -58,17 +137,5 @@ class TmuxDaemon:
 	def restart( self ):
 		self.stop()
 		self.start()
-
-def run( daemon, directive, *args ):
-	d = TmuxDaemon(daemon)
-	if hasattr(d, directive):
-		getattr(d, directive)(*args)
-	else:
-		raise Exception("Directive not found: {0}". format(directive))
-
-if __name__ == "__main__":
-	import sys
-	args = sys.argv[1:]
-	run(*args)
 
 # EOF - vim: tw=80 ts=4 sw=4 noet
